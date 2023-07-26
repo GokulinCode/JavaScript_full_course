0069509cf2514f5763abbd15693c7013f67e630b
GokulinCode <gokul@blockxsolutions.io>
2023-07-26 18:23:10 +0530

    video-4

---
+++ script.js
@@ -0,0 +1,46 @@
+console.log("new folder");
+
+// var let const
+
+// var variable
+// var name = "gokul"
+// var name = "pradap"
+
+
+// let variable
+// let name = "gokul";
+//  name = "pradap";
+
+// const variable
+// const name = "gokul";
+// const name = "pradap"
+// name = "pradap"
+
+
+let firstName = "gokul";
+let FirstName = "Gokul";
+let lastName = "pradap";
+let fullName = firstName +lastName;
+console.log(firstName +lastName);
+console.log(firstName +" "+lastName);
+console.log("my name is"+" "+firstName +lastName);
+console.log("my name is"+" "+fullName);
+
+
+//primitive type
+/*
+string
+number
+boolean
+undefined
+null
+*/
+//reference type
+// object
+//array
+//function
+let myName = "gokulpradap"; //string
+let age = 27; // number
+let isActive= true ; //boolean
+let person; //undefined
+let got = null;
